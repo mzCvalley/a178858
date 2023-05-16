8898b9444c42a81371e3f4b33f5ac8a485ff959c
Ming Ze <mztang0428@gmail.com>
2023-05-17 01:04:34 +0800

    Changes on Login Request

--- src/main/java/com/fyp/a178858/model/request/UserLoginRequest.java
+++ src/main/java/com/fyp/a178858/model/request/UserLoginRequest.java
@@ -7,6 +7,6 @@ import java.io.Serializable;
 @Data
 public class UserLoginRequest implements Serializable {
     private String username;
-    private String password;
+    private String Password;
 
 }
